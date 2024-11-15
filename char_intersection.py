c8e897e0692815d5a1c49d2ba33fabc80655223c
BlankyCat <mikatx2@hotmail.com>
2024-11-15 10:20:13 +0000

    Update char_intersection.py

--- char_intersection.py
+++ char_intersection.py
@@ -1,20 +1,22 @@
 
 Word1 = input()
 Word2 = input()
+ 
 
-intersection =""
+SetPalavra1 = set(Word1)
+SetPalavra2 = set(Word2)
 
-SetPalavras = set()
+Intersection = SetPalavra1.interception(SetPalavra2)
 
-for i in Word1:
-    for j in Word2:
-        if i == j:
-            SetPalavras.add(i)
+#for i in Word1:
+#    for j in Word2:
+#        if i == j:
+#            SetPalavras.add(i)
 
 for k in SetPalavras:
     intersection += k
 
 
-print(intersection)
+print(Intersection)
 
 
